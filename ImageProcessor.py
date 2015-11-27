809a60ab4519af22e6a2840883c8b4643cddea72
Joride on RaspbiB+ <kerrelinc@gmail.com>
2015-11-27 10:23:50 +0100

    improved structure of code and setup a way to take picture repeatedly

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,33 +1,27 @@
-import picamera
+
 import time
 from datetime import datetime
 
-time = datetime.now()
-filename = "capture-%04d%02d%02d-%02d%02d%02d.bmp" % (time.year, time.month, time.day, time.hour, time.minute, time.second)
-output = "/home/pi/motiondetection/%s" % filename
+# time = datetime.now()
+# filename = "capture-%04d%02d%02d-%02d%02d%02d.bmp" % (time.year, time.month, time.day, time.hour, time.minute, time.second)
+# output = "/home/pi/motiondetection/%s" % filename
 
-# instantiate the piCamera object, set some values
-camera = picamera.PiCamera()
-camera.framerate = 30
-camera.brightness = 50
-camera.rotation = 270 # valid values are: 0, 90, 180,  270
 
-# tell the camera to take a picture
-imageSize = (240, 135,)#(1920, 1080)
-camera.capture(output, format='bmp', use_video_port=False, resize=imageSize, splitter_port=0,)
+
+
 
 
 # process an image
-from PIL import Image
-image = Image.open(output)
-pixelData = image.load()
+# from PIL import Image
+# image = Image.open(output)
+# pixelData = image.load()
 
-imageSize = image.size
-x = imageSize[0]
-y = imageSize[1]
-print image.size
-print "y = %s" % y
-print "x = %s" % x
+# imageSize = image.size
+# x = imageSize[0]
+# y = imageSize[1]
+# print image.size
+# print "y = %s" % y
+# print "x = %s" % x
 
 # inspect each pixel's values
 # for xPixel in range(0, x):    
@@ -39,49 +33,70 @@ print "x = %s" % x
 #
 #
 from threading import Timer
+import picamera
 class PiCameraSnapshotter():
     delegate = None
     _isSnapshotting = False
+    _camera = None
     def __init__(
         self,
         repeats = False,
-        interval = 0):
+        interval = 1):
+        
         self.repeats = repeats
         self.interval = interval
-        self.thread = Timer(interval, self._timerFired, args=[])
      
     def _timerFired(self):
-        if self.repeats:
+        if self._isSnapshotting == True:
+            if self._camera == None:
+                # instantiate the piCamera object, set some values
+                self._camera = picamera.PiCamera()
+                self._camera.brightness = 50
+                self._camera.rotation = 270 # valid values are: 0, 90, 180,  270
+
+            # tell the camera to take a picture
+            imageSize = (240, 135,)#(1920, 1080)
             fileName = self.delegate.fileName()
-            print "fileName: %s" % fileName
-            self.thread = Timer(self.interval, self._timerFired, args=[])        
-            self.thread.start()
+            self._camera.capture(
+                fileName,
+                format='bmp',
+                use_video_port=False,
+                resize=imageSize,
+                splitter_port=0,)
+        else:
+            print "Timer fired but not taking a snapshot, because because stopSnapShotting() was called before the timer fired."
+    
+        if self.repeats and self._isSnapshotting == True:
+            thread = Timer(self.interval, self._timerFired, args=[])        
+            thread.start()
+        else:
+            print "Timer fired, but not starting a new timer, because because stopSnapShotting() was called while we were taking a snapshot."
 
     def startSnapShotting(self):
-        if self._isSnapshotting != True:
+        if self._isSnapshotting == False:
             self._isSnapshotting = True
-            self.thread.start()
+            thread = Timer(self.interval, self._timerFired, args=[])
+            thread.start()
 
     def stopSnapShotting(self):
         if self._isSnapshotting == True:
             self._isSnapshotting = False
-            self.thread.cancel()
+            self._camera = None
 
 
 import time
 class MotionDetector():
     def fileName(self):
         fileName = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        fileName = "%s.bmp" % fileName
         return fileName
  
     def startDetecting(self):
-        snapShotter = PiCameraSnapshotter(repeats = True, interval = 0.5)
+        snapShotter = PiCameraSnapshotter(repeats = True, interval = 1.0)
         snapShotter.delegate = self
-
-        print "starting to snapshot %s" % datetime.now().second
+ 
         snapShotter.startSnapShotting()
         time.sleep(10)
-        print "stopping snapshotting %s" % datetime.now().second
         snapShotter.stopSnapShotting()
 
 detector = MotionDetector()
